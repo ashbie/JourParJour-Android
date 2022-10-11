5a491f0bdfb2ccac28cd14fc4283017af5c7bb73
ashbie <ashbie.b.mweemba@gmail.com>
2022-10-11 01:07:42 +0100

    prepare the apps different paths/activities

---
+++ app/src/main/java/kas/du/jourparjour/ListingsActivity.java
@@ -0,0 +1,14 @@
+package kas.du.jourparjour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listings);
+    }
+}
